d32e1c23f19b97c38eb7c49c27cdfb57a65e6e69
JosefinDH <153727579+JosefinDH@users.noreply.github.com>
2024-01-09 19:55:31 +0100

    Add files via upload

--- modellering_av_ozonkolumner_GA.py
+++ modellering_av_ozonkolumner_GA.py
@@ -2,32 +2,28 @@ import netCDF4 as nc
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Öppnade av datafilen
 fn = r"openmars_ozo_my27_ls31_my27_ls45.nc"
 ds = nc.Dataset(fn)
-
-#För att kunna se datafilens dimensioner och variabler -->
 #for dim in ds.dimensions.values():
     #print(dim)
 #for var in ds.variables.values():
     #print(var)
-
-#Indexering av variabler från datafilen
 lats = ds.variables['lat'][:]
 lons = ds.variables['lon'][:]
-#time = ds.variables['time'][:]
-#Ls = ds.variables["Ls"][:]
-#MY = ds.variables['MY'][:]
+time = ds.variables['time'][:]
+Ls = ds.variables["Ls"][:]
+MY = ds.variables['MY'][:]
 
-#Val av intervall för longitud och latitud
 latselect = np.logical_and(lats>-60, lats<60)
 
 lonselect = np.logical_and(lons>-135,lons<135)
+#Samma sak för övriga varabler!
+# data = ds.variables['o3col'][0, latselect, lonselect]
 
-#Skapande av grafiska modeller
 plt.figure(figsize=(8, 6))
 contour = plt.contourf(lons, lats, ds.variables["o3col"][0,:,:], levels=30, cmap="nipy_spectral")
 plt.colorbar(contour, label='µm-atm(mikrometer-atm)')
+#plt.contourf(data[::-1])
 
 plt.xlabel('Longitud')
 plt.ylabel('Latitud')
